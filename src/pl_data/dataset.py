19c4b570db2e0ee60770a9feaa45ebd908580709
Drew Linsley <drewlinsley@gmail.com>
2022-03-16 15:44:47 -0400

    Added nuclear gedi experiment.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -156,7 +156,7 @@ class NuclearGedi(Dataset):
     def __getitem__(self, index: int):
         img = self.files[index]
         label = self.labels[index]
-        # label = label.reshape(-1, 1)
+        label = label.reshape(-1)
         img = io.imread(img, plugin='pil')
         img = img.astype(np.float32)
         img = (img - self.minval) / self.denom  # Normalize to [0, 1]
